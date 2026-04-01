86a9d831a92b3fd886decacc247d8da78791b0cd
bed-com-45-24 <bed-com-45-24@unima.ac.mw>
2026-04-01 00:06:43 -0700

    SwitchUsersSubscription.cpp

---
+++ SwitchUsersSubscription.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int daysUntilExpiration;
+    daysUntilExpiration = (7 * 3 + 5) % 12;
+    cout << "Days until expiration: " << daysUntilExpiration << endl;
+     switch (daysUntilExpiration) 
+     {
+        case 0: 
+            cout << "Your subscription has expired." << endl;
+              break;
+        case 1:
+            cout << "Your subscription expiresx within a day!" << endl;
+            cout << "Renew now and save 20%." << endl;
+              break;
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+            cout << "Your expires in " << daysUntilExpiration << "days" << endl;
+            cout << "Renew now and save 10%."<< endl;
+               break;
+        case 6:
+        case 7:
+        case 8:
+        case 9:
+        case 10:
+            cout << "Your subscription will expire soon. Renew now!" << endl;
+               break;
+        default:
+            cout << "You have an active subscription." << endl;       
+
+     }
+
+     return 0;
+}
